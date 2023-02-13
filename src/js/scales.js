fcb76acca58506bec61e7b87831705dfbd75b8b6
Anne-Marie Dufour <annema.dufour@gmail.com>
2023-02-13 15:02:07 +0100

    Updates

--- src/js/scales.js
+++
@@ -1,6 +0,0 @@
-import { scaleOrdinal } from "d3-scale";
-import { languageFamilies } from "./helper";
-
-export const colorScale = scaleOrdinal()
-  .domain(languageFamilies.map(d => d.label))
-  .range(languageFamilies.map(d => d.color));
